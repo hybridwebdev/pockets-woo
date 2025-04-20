f76769a8b05be8e23d96e5935aa159493eb3d2b3
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2025-04-20 01:19:01 -0600

    tweaked woo stuff

--- crud/models/woo/products/read.php
+++ crud/models/woo/products/read.php
@@ -1,9 +1,9 @@
 <?php
 namespace pockets_woo\crud\models\woo\products;
 
-class read extends \pockets\crud\resource_walker {
+class read extends \pockets\crud\models\posts\read {
 
-     function items( array $read ) : array {
+    function items( array $read ) : array {
         return array_map( 
             array: $this->resource->posts ?? [],
             callback: fn( $post ) => \pockets::crud( 'woo/product' )::init( $post )->read( $read ), 
--- plugin/module.php
+++ plugin/module.php
@@ -17,6 +17,25 @@ class module extends \pockets\base {
         api\module::init();
         \pockets_woo\crud\models\woo\module::init();
 
+        add_filter( 'woocommerce_locate_template', function( $template, $template_name, $template_path ){
+
+            // //echo $template;
+
+            // $path = sprintf( "%s/%s/%s", \pockets_woo\base::init()->dir, 'templates/woocommerce',  $template_name);
+
+            // $debug = fn( $color ) => sprintf("<div class='p-4 %s'><p>%s</p><p>%s</p><p>%s</p> </div>", $color, $path, $template_path, $template_name);
+
+            // if( file_exists($path) ){
+            //     //echo $debug('bg-black text-white');
+            //     return $path;
+            // }
+
+            // //echo $debug('bg-primary text-white');
+
+            // return $template;
+            
+        }, 10, 3 );
+
     }
     
 }
