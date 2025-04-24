0382c57e18736cf7025be361bad4f7ab155169f8
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2025-04-24 04:46:13 -0600

    its still wonkey

--- plugin/route-filters.php
+++ plugin/route-filters.php
@@ -20,7 +20,7 @@ class route_filters {
 
         if( $d['crud_resource']['name'] == 'product') {
 
-            return \pockets::crud('post')::init(wc_get_page_id('shop'))->read( [
+            return \pockets::crud('post')::init( wc_get_page_id('shop') )->read( [
                 'crud_resource'
             ] ) + [
                 'metaKey' => "body"
