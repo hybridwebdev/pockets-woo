ffc89f5cf6691e80907010a0570c7325d20f8e8d
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2025-04-24 04:31:49 -0600

    i think this might be good

--- plugin/route-filters.php
+++ plugin/route-filters.php
@@ -10,28 +10,26 @@ class route_filters {
     use \pockets\traits\initOnce;
 
     function __construct(){
-
-        add_filter("pockets-node-tree/router/get/array_by_resolved/wp_post_type", [ $this, 'wp_post_type' ], 10, 2 );
-       
+        add_filter('pockets-node-tree/router/read/document_tree_source/wp_post_type', [$this, 'wp_post_type'], 10, 2);
     }
 
-    /**
-        Modify instance so if it's a product archive, it points to shop page instead. 
-    */    
-    function wp_post_type( $crudModel ){
+    
+    function wp_post_type( $source, $resource ){
 
-        $source = $crudModel->read( [ 'crud_resource' ] );
+        $d =  $resource->read( [ "crud_resource" ] );
 
-        if( $source['crud_resource']['name'] == 'product' ) {
-            
-            return \pockets::crud('node-tree/router')::init( [
-                'url' => get_permalink( wc_get_page_id( 'shop' ) )
-            ] )->resource;
+        if( $d['crud_resource']['name'] == 'product') {
+
+            return \pockets::crud('post')::init(wc_get_page_id('shop'))->read( [
+                'crud_resource'
+            ] ) + [
+                'metaKey' => "body"
+            ];
 
         }
 
-        return $crudModel;
-
+        return $source;
+        
     }
 
 }
