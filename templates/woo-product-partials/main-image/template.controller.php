dd0a03072e6e7a9e5897f0376c3f9156889a62eb
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2025-05-07 01:21:49 -0600

    deleted partials. Moved to template loader

--- templates/woo-product-partials/main-image/template.controller.php
+++ templates/woo-product-partials/main-image/template.controller.php
@@ -1,9 +1,14 @@
-<label class='grid-info-120'>
+<label class='grid-info-120' v-tooltip='"Adds class to image"'>
     <span>Image Class</span>
     <input v-model='data.class' class='form-control'>
 </label>
 
-<label class='grid-info-120'>
+<label class='grid-info-120' v-tooltip='"Image size to use"'>
     <span>Image Size</span>
     <input v-model='data.size' class='form-control'>
+</label>
+
+<label class='grid-info-120' v-tooltip='"Fallback url to use when product has no set image"'>
+    <span>Fallback Image</span>
+    <input v-model='data.fallback' class='form-control'>
 </label>
