1928a77ffcc59076f6c50901827ec043884115a6
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2025-05-09 06:02:35 -0600

    rebuild

--- template-hooks/checkout-form.php
+++ template-hooks/checkout-form.php
@@ -6,7 +6,17 @@ class checkout_form {
     use \pockets\traits\init;
 
     function __construct(){
+        
+        // add_filter( 'woocommerce_checkout_cart_item_quantity', function($quantity_html, $cart_item, $cart_item_key){
 
+        //     return sprintf(
+        //         <<<HTML
+        //             <strong class="product-quantity">%s</strong>
+        //         HTML,
+        //         $cart_item['quantity']
+        //     );
+        // }, 10, 3 );
+        
     }
 
 
