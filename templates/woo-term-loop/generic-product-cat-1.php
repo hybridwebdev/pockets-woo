4f7cea09b898113e7a13f787a7cb872270c5c8f8
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2025-05-11 01:01:05 -0600

    created term node

---
+++ templates/woo-term-loop/generic-product-cat-1.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+* Template Name: Generic Product Cat 1
+* Template Type: term
+*/
+
+$data = $this->read_resource([
+    'name',
+    'link',
+    "ID"
+]); 
+
+$thumbnail_id = get_term_meta( $data['ID'], 'thumbnail_id', true );
+
+$image_url = wc_placeholder_img_src( 'thumbnail' );
+
+if ( $thumbnail_id ) {
+    $image_url = wp_get_attachment_url( $thumbnail_id );
+}
+
+?>
+<div class='term-loop-item'>
+    <div class='grid-info-100'>
+        <a href='<?= $data['link']?>'>
+            <img src='<?= $image_url?>' class='img-fluid'>
+        </a>
+        <a href='<?= $data['link']?>'>
+            <?= $data['name'] ?>
+        </a>
+    </div>
+</div>
